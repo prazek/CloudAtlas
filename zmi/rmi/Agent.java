c2bd8fd60a5ce1b19eb58a583f00b9d2d72daafd
Piotr Padlewski <piotr.padlewski@gmail.com>
2017-11-12 16:51:14 +0100

    fix

--- zmi/rmi/Agent.java
+++ zmi/rmi/Agent.java
@@ -4,13 +4,22 @@ import model.ZMI;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.util.HashSet
+import java.util.HashSet;
 
 public class Agent implements Remote {
-    public HashSet<ZMI> zones() {}
-    public ZMI zone(String zoneName) {}
-    public void installQuery(String query) {}
-    public void uninstallQuery(String query) {}
-    // or string?
-    public void setZoneValue(String zoneName) {}
+    public HashSet<ZMI> zones() throws RemoteException {
+        return null;
+    }
+    public ZMI zone(String zoneName) throws RemoteException {
+        return null;
+    }
+    public void installQuery(String query) throws RemoteException {
+
+    }
+    public void uninstallQuery(String query) throws RemoteException {
+
+    }
+    public void setZoneValue(String zoneName) throws RemoteException {
+
+    }
 }
--- zmi/rmi/Client.java
+++ zmi/rmi/Client.java
@@ -7,13 +7,13 @@ import java.rmi.server.UnicastRemoteObject;
 
 
 
-public class Server {
+public class Client {
     public static void main(String[] args) {
         if (System.getSecurityManager() == null) {
             System.setSecurityManager(new SecurityManager());
         }
         try {
-
+            
 
         } catch (Exception e) {
             System.err.println("ComputeEngine exception:");
