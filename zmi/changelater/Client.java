f3a12acb095d0f4999f6e5c9a99efc046ede9111
Stanisław Barzowski <stanislaw.barzowski@gmail.com>
2017-11-14 20:07:06 +0100

    Client HttpServer

--- zmi/changelater/Client.java
+++ zmi/changelater/Client.java
@@ -1,17 +1,48 @@
 package changelater;
 
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class Client {
     public static void main(String[] args) {
         if (System.getSecurityManager() == null) {
             System.setSecurityManager(new SecurityManager());
         }
         try {
-
-
+            HttpServer server = HttpServer.create(new InetSocketAddress(8042), 0);
+            server.createContext("/", new MainPage());
+            server.createContext("/zmi/", new ZMIPage());
+            server.setExecutor(null); // creates a default executor
+            server.start();
         } catch (Exception e) {
             System.err.println("ComputeEngine exception:");
             e.printStackTrace();
         }
     }
+    private static class MainPage implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            t.getResponseHeaders().add("Location", "/zmi/");
+            t.sendResponseHeaders(302, 0);
+        }
+    }
+    private static class ZMIPage implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "<h1>Main page</h1>" +
+                    String.format("<p>%s</p>", t.getRequestURI().getRawPath());
+            t.getResponseHeaders().add("Content-Type", "text/html");
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
 }
+
--- zmi/changelater/WWWServer.java
+++ zmi/changelater/WWWServer.java
@@ -1,9 +1,5 @@
 package changelater;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 public class WWWServer {
 
 
