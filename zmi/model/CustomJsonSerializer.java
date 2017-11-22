bf7bfca2dca81de043a4a9afd1e2c14c95164a88
Piotr Padlewski <piotr.padlewski@gmail.com>
2017-11-22 14:39:43 +0100

    Full custom serializer

--- zmi/model/CustomJsonSerializer.java
+++ zmi/model/CustomJsonSerializer.java
@@ -7,26 +7,110 @@ import java.util.Map;
 
 public class CustomJsonSerializer {
     static public class ValueIntAdapter implements JsonSerializer<ValueInt> {
-
         @Override
         public JsonElement serialize(ValueInt src, Type typeOfSrc,
                                      JsonSerializationContext context) {
             JsonObject obj = new JsonObject();
-            obj.addProperty("type", "ValueInt");
+            obj.addProperty("type", src.getClass().getSimpleName());
             obj.addProperty("value", src.getValue());
             return obj;
         }
     }
 
-
-    static public class ValueListAdapter implements JsonSerializer<ValueList> {
-
+    static public class ValueBooleanAdapter implements JsonSerializer<ValueBoolean> {
         @Override
-        public JsonElement serialize(ValueList src, Type typeOfSrc,
+        public JsonElement serialize(ValueBoolean src, Type typeOfSrc,
+                                     JsonSerializationContext context) {
+            JsonObject obj = new JsonObject();
+            obj.addProperty("type", src.getClass().getSimpleName());
+            obj.addProperty("value", src.getValue());
+            return obj;
+        }
+    }
+    static public class ValueDoubleAdapter implements JsonSerializer<ValueDouble> {
+        @Override
+        public JsonElement serialize(ValueDouble src, Type typeOfSrc,
+                                     JsonSerializationContext context) {
+            JsonObject obj = new JsonObject();
+            obj.addProperty("type", src.getClass().getSimpleName());
+            obj.addProperty("value", src.getValue());
+            return obj;
+        }
+    }
+
+    static public class ValueStringAdapter implements JsonSerializer<ValueString> {
+        @Override
+        public JsonElement serialize(ValueString src, Type typeOfSrc,
                                      JsonSerializationContext context) {
 
             JsonObject obj = new JsonObject();
-            obj.addProperty("type", "ValueList");
+            obj.addProperty("type", src.getClass().getSimpleName());
+            obj.addProperty("value", src.getValue());
+            return obj;
+        }
+    }
+
+    static public class ValueNullAdapter implements JsonSerializer<ValueNull> {
+        @Override
+        public JsonElement serialize(ValueNull src, Type typeOfSrc,
+                                     JsonSerializationContext context) {
+            JsonObject obj = new JsonObject();
+            obj.addProperty("type", src.getClass().getSimpleName());
+            return obj;
+        }
+    }
+
+    static public class ValueDurationAdapter implements JsonSerializer<ValueDuration> {
+        @Override
+        public JsonElement serialize(ValueDuration src, Type typeOfSrc,
+                                     JsonSerializationContext context) {
+            JsonObject obj = new JsonObject();
+            obj.addProperty("type", src.getClass().getSimpleName());
+            obj.addProperty("value", src.getValue());
+            return obj;
+        }
+    }
+
+    static public class ValueTimeAdapter implements JsonSerializer<ValueTime> {
+        @Override
+        public JsonElement serialize(ValueTime src, Type typeOfSrc,
+                                     JsonSerializationContext context) {
+            JsonObject obj = new JsonObject();
+            obj.addProperty("type", src.getClass().getSimpleName());
+            obj.addProperty("value", src.getValue());
+            return obj;
+        }
+    }
+
+    static public class ValueContactAdapter implements JsonSerializer<ValueContact> {
+        @Override
+        public JsonElement serialize(ValueContact src, Type typeOfSrc,
+                                     JsonSerializationContext context) {
+            JsonObject obj = new JsonObject();
+            obj.addProperty("type", src.getClass().getSimpleName());
+            obj.addProperty("name", src.getName().getName());
+            obj.addProperty("address", src.getAddress().toString());
+            return obj;
+        }
+    }
+
+    static public class PathNameAdapter implements JsonSerializer<PathName> {
+        @Override
+        public JsonElement serialize(PathName src, Type typeOfSrc,
+                                     JsonSerializationContext context) {
+            JsonObject obj = new JsonObject();
+            obj.addProperty("type", src.getClass().getSimpleName());
+            obj.addProperty("name", src.getName());
+            return obj;
+        }
+    }
+
+    static public class ValueSetAdapter implements JsonSerializer<ValueSet> {
+        @Override
+        public JsonElement serialize(ValueSet src, Type typeOfSrc,
+                                     JsonSerializationContext context) {
+            JsonObject obj = new JsonObject();
+            obj.addProperty("type", src.getClass().getSimpleName());
             JsonArray array = new JsonArray();
             for (Value v : src.getValue()) {
                 final JsonElement value = context.serialize(v);
@@ -38,18 +122,24 @@ public class CustomJsonSerializer {
         }
     }
 
-    static public class ValueStringAdapter implements JsonSerializer<ValueString> {
-
+    static public class ValueListAdapter implements JsonSerializer<ValueList> {
         @Override
-        public JsonElement serialize(ValueString src, Type typeOfSrc,
+        public JsonElement serialize(ValueList src, Type typeOfSrc,
                                      JsonSerializationContext context) {
 
             JsonObject obj = new JsonObject();
-            obj.addProperty("type", "ValueString");
-            obj.addProperty("value", src.getValue());
+            obj.addProperty("type", src.getClass().getSimpleName());
+            JsonArray array = new JsonArray();
+            for (Value v : src.getValue()) {
+                final JsonElement value = context.serialize(v);
+                array.add(value);
+            }
+
+            obj.add("value", array);
             return obj;
         }
     }
+
 
     static public class AttributeAdapter implements JsonSerializer<Attribute> {
         @Override
@@ -90,7 +180,7 @@ public class CustomJsonSerializer {
 
             final JsonElement attributes = context.serialize(src.getAttributes());
             obj.add("attributes", attributes);
-            
+
             return obj;
         }
     }
@@ -98,7 +188,18 @@ public class CustomJsonSerializer {
     static public Gson getSerializer() {
         GsonBuilder gsonBuilder = new GsonBuilder().setPrettyPrinting();
         gsonBuilder.registerTypeAdapter(ValueInt.class, new ValueIntAdapter());
+        gsonBuilder.registerTypeAdapter(ValueString.class, new ValueStringAdapter());
+        gsonBuilder.registerTypeAdapter(ValueDouble.class, new ValueDoubleAdapter());
+        gsonBuilder.registerTypeAdapter(ValueBoolean.class, new ValueBooleanAdapter());
+        gsonBuilder.registerTypeAdapter(ValueNull.class, new ValueNullAdapter());
+        gsonBuilder.registerTypeAdapter(ValueDuration.class, new ValueDurationAdapter());
+        gsonBuilder.registerTypeAdapter(ValueTime.class, new ValueTimeAdapter());
+        gsonBuilder.registerTypeAdapter(ValueContact.class, new ValueContactAdapter());
+        gsonBuilder.registerTypeAdapter(PathName.class, new PathNameAdapter());
+
         gsonBuilder.registerTypeAdapter(ValueList.class, new ValueListAdapter());
+        gsonBuilder.registerTypeAdapter(ValueSet.class, new ValueSetAdapter());
+
         gsonBuilder.registerTypeAdapter(ZMI.class, new ZMIAdapter());
         gsonBuilder.registerTypeAdapter(Attribute.class, new AttributeAdapter());
         gsonBuilder.registerTypeAdapter(AttributesMap.class, new AttributesMapAdapter());
--- zmi/model/CustomJsonSerializerTests.java
+++ zmi/model/CustomJsonSerializerTests.java
@@ -1,18 +1,70 @@
 package model;
 
 import com.google.gson.Gson;
+import interpreter.TestHierarchy;
 
 import static org.junit.Assert.assertEquals;
 
 public class CustomJsonSerializerTests {
 
     @org.junit.Test
-    public void testRegexpr() throws Exception {
+    public void testSimple() throws Exception {
         ZMI zmi = new ZMI();
         zmi.getAttributes().add("abc", new ValueInt(42L));
         Gson gson = CustomJsonSerializer.getSerializer();
 
         String result = gson.toJson(zmi);
-        assertEquals("{ \"type\"", result);
+        assertEquals("{\n" +
+                "  \"type\": \"ZMI\",\n" +
+                "  \"attributes\": {\n" +
+                "    \"type\": \"AttributesMap\",\n" +
+                "    \"values\": {\n" +
+                "      \"abc\": {\n" +
+                "        \"type\": \"ValueInt\",\n" +
+                "        \"value\": 42\n" +
+                "      }\n" +
+                "    }\n" +
+                "  }\n" +
+                "}", result);
     }
+
+    @org.junit.Test
+    public void testComplex() throws Exception {
+        ZMI zmi = TestHierarchy.createTestHierarchy();
+        Gson gson = CustomJsonSerializer.getSerializer();
+
+        String result = gson.toJson(zmi);
+        assertEquals("{\n" +
+                "  \"type\": \"ZMI\",\n" +
+                "  \"attributes\": {\n" +
+                "    \"type\": \"AttributesMap\",\n" +
+                "    \"values\": {\n" +
+                "      \"owner\": {\n" +
+                "        \"type\": \"ValueString\",\n" +
+                "        \"value\": \"/uw/violet07\"\n" +
+                "      },\n" +
+                "      \"level\": {\n" +
+                "        \"type\": \"ValueInt\",\n" +
+                "        \"value\": 0\n" +
+                "      },\n" +
+                "      \"name\": {\n" +
+                "        \"type\": \"ValueString\"\n" +
+                "      },\n" +
+                "      \"contacts\": {\n" +
+                "        \"type\": \"ValueSet\",\n" +
+                "        \"value\": []\n" +
+                "      },\n" +
+                "      \"cardinality\": {\n" +
+                "        \"type\": \"ValueInt\",\n" +
+                "        \"value\": 0\n" +
+                "      },\n" +
+                "      \"timestamp\": {\n" +
+                "        \"type\": \"ValueTime\",\n" +
+                "        \"value\": 1352488217342\n" +
+                "      }\n" +
+                "    }\n" +
+                "  }\n" +
+                "}", result);
+    }
+
 }
