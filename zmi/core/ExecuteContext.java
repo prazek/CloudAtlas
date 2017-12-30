2953a861c6e7b6b4e178240e08e33ae04a719b0b
Piotr Padlewski <piotr.padlewski@gmail.com>
2017-12-30 14:47:49 +0100

    Add Executor

---
+++ zmi/core/ExecuteContext.java
@@ -0,0 +1,6 @@
+package core;
+
+public class ExecuteContext {
+    Executor sender;
+    Object data;
+}
---
+++ zmi/core/Executor.java
@@ -0,0 +1,24 @@
+package core;
+
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public abstract class Executor {
+    private BlockingQueue<ExecuteContext> tasks = new ArrayBlockingQueue<>(100);
+
+    public final void run() throws InterruptedException {
+        while (true) {
+            ExecuteContext context = tasks.take();
+            execute(context);
+        }
+    }
+
+
+    public final void pushToExecute(ExecuteContext context) {
+        tasks.add(context);
+    }
+
+    public abstract void execute(ExecuteContext context);
+
+}
