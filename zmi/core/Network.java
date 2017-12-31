f257618b3f461bf36bd2181f760fb346e5ee32ad
Piotr Padlewski <piotr.padlewski@gmail.com>
2017-12-31 18:49:10 +0100

    sending and receiving

--- zmi/core/Network.java
+++ zmi/core/Network.java
@@ -1,8 +1,24 @@
 package core;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 public class Network extends Executor {
 
     private DatabaseUpdater databaseUpdater = null;
+    private DatagramSocket socket;
+
+    Network() {
+        try {
+            // TODO make it configurable
+            socket = new DatagramSocket(2137);
+        } catch (SocketException ex) {
+            System.err.println("Socket exception: " + ex);
+            assert false;
+        }
+    }
 
 
     void setDatabaseUpdater(DatabaseUpdater databaseUpdater) {
@@ -13,5 +29,19 @@ public class Network extends Executor {
     @Override
     void execute(ExecuteContext context) {
 
+    }
+
+    void receive() throws IOException {
+        byte buf[] = new byte[256];
+        DatagramPacket packet
+                = new DatagramPacket(buf, buf.length);
+        socket.receive(packet);
+    }
+
+
+    void sendMsg(byte bytes[]) throws IOException {
+        DatagramPacket packet
+                = new DatagramPacket(bytes, bytes.length);
+        socket.send(packet);
     }
 }
