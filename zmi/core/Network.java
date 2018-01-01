0c6e305e041656a2da3e286436a3ba9ea8d781d7
Piotr Padlewski <piotr.padlewski@gmail.com>
2018-01-01 19:25:39 +0100

    More network

--- zmi/core/Network.java
+++ zmi/core/Network.java
@@ -1,7 +1,9 @@
 package core;
 
 
+import model.ValueContact;
 import model.ZMI;
+import sun.misc.Request;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
@@ -10,6 +12,8 @@ import java.net.InetAddress;
 import java.net.SocketException;
 import java.security.Timestamp;
 import java.util.Arrays;
+import java.util.Currency;
+import java.util.HashMap;
 import java.util.Map;
 
 import static java.lang.Thread.sleep;
@@ -18,6 +22,8 @@ public class Network extends Executor {
 
     private DatabaseUpdater databaseUpdater = null;
     private DatagramSocket receivingSocket;
+    private Map<Long, RequestExtraData> requestsExtraData = new HashMap<>();
+
 
     Network() {
         try {
@@ -43,15 +49,23 @@ public class Network extends Executor {
     void execute(ExecuteContext context) {
 
     }
+    class Message {
+        Message(core.Gossip.GossipMessageUDP messageUDP, InetAddress senderAddress) {
+            this.messageUDP = messageUDP;
+            this.senderAddress = senderAddress;
+        }
+        core.Gossip.GossipMessageUDP messageUDP;
+        InetAddress senderAddress;
+    }
 
-    core.Gossip.GossipMessageUDP receive() throws IOException {
+    Message receive() throws IOException {
         byte buf[] = new byte[2137];
         DatagramPacket packet
                 = new DatagramPacket(buf, buf.length);
         receivingSocket.receive(packet);
 
         byte buf2[] = Arrays.copyOf(buf, packet.getLength());
-        return core.Gossip.GossipMessageUDP.parseFrom(buf2);
+        return new Message(core.Gossip.GossipMessageUDP.parseFrom(buf2), packet.getAddress());
     }
 
 
@@ -63,15 +77,69 @@ public class Network extends Executor {
     }
 
 
+    class RequestExtraData {
+        RequestExtraData(long requestTimestamp,
+                         long requestReceivedTimestamp,
+                         InetAddress address) {
+            this.requestTimestamp = requestTimestamp;
+            this.requestReceivedTimestamp = requestReceivedTimestamp;
+            this.address = address;
+        }
+
+        long requestTimestamp;
+        long requestReceivedTimestamp;
+        InetAddress address;
+    }
+
+
+
+
+
+
+
+    private void handleGossipingResponseFromDB(Gossip.GossipingResponseFromDB response) {
+        // TODO
+        RequestExtraData extraData = requestsExtraData.get(2137L);
+
+        Gossip.GossipResponseUDP responseUDP = Gossip.GossipResponseUDP.newBuilder()
+                .setResponseTimestamp(System.currentTimeMillis())
+                .setAttributesMap(response.getAttributesMap())
+                .putAllFreshness(response.getFreshnessMap())
+                .setRequestReceivedTimestamp(extraData.requestReceivedTimestamp)
+                .setRequestTimestamp(extraData.requestTimestamp).build();
+
+        try {
+            sendMsg(Gossip.GossipMessageUDP.newBuilder().setGossipResponseUDP(responseUDP).build(), extraData.address);
+        } catch(IOException ex) {
+            // TODO redo 5 times?
+        }
+    }
+
+    private void handleGossipingRequestFromDB(Gossip.GossipingRequestFromDB request) {
+        Gossip.GossipRequestUDP requestUDP = Gossip.GossipRequestUDP.newBuilder()
+                .setRequestTimestamp(System.currentTimeMillis()).build();
+
+        ValueContact contact = ValueContact.fromProtobuf(request.getContact());
+        // TODO set any data indicating that we are waiting for response?
+
+        try {
+            sendMsg(Gossip.GossipMessageUDP.newBuilder().setGossipRequestUDP(requestUDP).build(),
+                    contact.getAddress());
+        } catch (IOException ex) {
+            // TODO handle
+        }
+    }
+
+
     class Receiver implements Runnable {
 
         @Override
         public void run() {
             while (true) {
                 try {
-                    core.Gossip.GossipMessageUDP gossip = receive();
-                    System.out.println("Got gossip: "+ gossip.toString());
-                    handleMessage(gossip);
+                    Message message = receive();
+                    System.out.println("Got message from " + message.senderAddress.toString());
+                    handleMessage(message);
 
                     try {
                         sleep(3*1000);
@@ -84,30 +152,31 @@ public class Network extends Executor {
             }
         }
 
-        void handleMessage(core.Gossip.GossipMessageUDP message) {
-            if (message.hasGossipRequestUDP()) {
-
+        private void handleMessage(Message message) {
+            if (message.messageUDP.hasGossipRequestUDP()) {
+                handleGossipRequest(message.messageUDP.getGossipRequestUDP(), message.senderAddress);
             }
-            else if (message.hasGossipResponseUDP()) {
-
+            else if (message.messageUDP.hasGossipResponseUDP()) {
+                handleGossipResponse(message.messageUDP.getGossipResponseUDP());
             }
             else {
                 System.err.println("Invalid message" + message.toString());
             }
         }
+
         //TODO wtf is rtd
-        long rtd(long requestTimestamp, long requestReceivedTimestamp,
+        private long rtd(long requestTimestamp, long requestReceivedTimestamp,
                        long responseTimestamp, long responseReceivedTimestamp) {
             return (responseReceivedTimestamp - requestTimestamp)
                     + (responseTimestamp - requestReceivedTimestamp);
         }
 
-        long timeDelta(long responseTimestamp, long rtd, long responseReceivedTimestamp) {
+        private long timeDelta(long responseTimestamp, long rtd, long responseReceivedTimestamp) {
             return responseTimestamp + (long)(0.5*rtd) - responseReceivedTimestamp;
         }
 
 
-        void updateFreshnessMap( Map<String, Long> freshness, long timeDelta) {
+        private void updateFreshnessMap(Map<String, Long> freshness, long timeDelta) {
             for (Map.Entry<String, Long> entry : freshness.entrySet()) {
                 entry.setValue(entry.getValue() + timeDelta);
             }
@@ -129,11 +198,21 @@ public class Network extends Executor {
 
 
             // Send updateDatabase to database
-
         }
 
-        void handleGossipRequest(core.Gossip.GossipRequestUDP request) {
+        void handleGossipRequest(core.Gossip.GossipRequestUDP request, InetAddress address) {
+
+            // TODO
+            int id = 42;
+            requestsExtraData.put(42L,
+                    new RequestExtraData(request.getRequestTimestamp(), System.currentTimeMillis(), address));
+
+
+            Database.CurrentDatabaseRequest databaseRequest =
+                    Database.CurrentDatabaseRequest.newBuilder().build();
             // ASK database for data
+
+
         }
     }
 
